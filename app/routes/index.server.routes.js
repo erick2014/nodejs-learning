1f645de8db572ae1cc73a1ceff54fc33ac19dd98
erick2014 <erickarmandogarcia@hotmail.com>
2015-04-30 17:15:01 -0500

    Fix controller path into index.server.routes.js file.

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,4 +1,4 @@
 module.exports=function(app){
-  const index=require("../controller/index.server.controller");
+  const index=require("../controllers/index.server.controller");
   app.get('/',index.render);
 }
