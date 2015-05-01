8874a1af32b4f21bc9c29e0fd8fb35b593f762af
Erick Garcia <erickarmandogarcia@hotmail.com>
2015-05-01 16:09:21 -0500

    Add aditional modules into express config file.

--- config/express.js
+++ config/express.js
@@ -1,8 +1,37 @@
+//express module
 const express=require('express');
+//this provides a simple logger middleware
+const morgan=require("morgan");
+//this provides response compression
+const compress=require("compression");
+// this provides several middlewares to handle request data
+const bodyParser=require("body-parser");
+//this provides delete and put verbs
+const methodOverride=require("method-override");
 
 module.exports=function(){
   //get an express instance
   const app=express();
+  //check the current environment
+  if( process.env.NODE_ENV === 'development' ){
+    //here we load the morgan middleware
+    app.use(morgan('dev'));
+  }
+  else if( process.env.NODE_ENV ==='production' ){
+    //here we load the compress middleware
+    app.use(compress())
+  }
+
+  //load the urlencode method from bodyParser
+  app.use(bodyParser.urlencoded({
+    extended:true
+  }));
+
+  //load the json method from body parser
+  app.use(bodyParser.json());
+  //load the override middleware
+  app.use(methodOverride());
+
   //include the index route
   require("../app/routes/index.server.routes.js")(app);
 
