71f9eb12861377231e1d46d9610cfbe287ee95f0
erick2014 <erickarmandogarcia@hotmail.com>
2015-05-02 14:34:24 -0500

    Add views property to the session, into index controller.

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,4 +1,14 @@
 /*Index controller */
 exports.render=function(req,res){
+  var sess=req.session
+  //well here, im trying to get the view property from session
+  if(sess.views){
+    sess.views++;
+    console.log(`visit number ${sess.views}`);
+  }
+  //setting a new property's value if doesnt exists
+  else{
+    sess.views=1;
+  }
   res.render('index',{title:"Index Page",content:"amm some text over here.."});
 }
