4ce0b772c46e8723fce0d16d77739ad0c83b6aba
erick2014 <erickarmandogarcia@hotmail.com>
2015-05-03 16:49:07 -0500

    Add the mongose module.

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 //if there is not an environment then set it
 process.env.NODE_ENV= process.env.NODE_ENV || 'development'
-//set a port to listen to
-const port=3000;
+//include the mongoose config file to get an instance
+const mongoose=require('./config/mongoose')(); 
+
 /*include the configuration file
   also load the route and its controller
 */
 const app=require("./config/express")();
+//set a port to listen to
+const port=3000;
 
 //port to listen to
 app.listen(port);
